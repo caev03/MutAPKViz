c4303cf0de846b9dc9329dc7ee3201056328b9ff
caev03 <ca.escobar2434@uniandes.edu.co>
2018-05-15 09:54:08 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,147 @@
+var width = 700,
+    height = 720,
+    radius = Math.min(width, height) / 2;
+
+		var x = d3.scale.linear()
+    .range([0, 2 * Math.PI]);
+
+var y = d3.scale.sqrt()
+    .range([0, radius]);
+
+var color = d3.scale.category20c();
+
+var svg = d3.select("#mySVG")
+    .attr("width", width)
+    .attr("height", function(){return height+10})
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + ((height / 2)+5) + ")");
+
+var partition = d3.layout.partition()
+    .sort(null)
+    .value(function(d) { return d.size; });
+
+var arc = d3.svg.arc()
+    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
+    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
+    .innerRadius(function(d) { return Math.max(0, y(d.y)); })
+    .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
+
+// Keep track of the node that is currently being displayed as the root.
+var node;
+
+d3.json("data.json", function(error, root) {
+  console.log(root);
+  node = root;
+  var path = svg.datum(root).selectAll("path")
+      .data(partition.nodes)
+    .enter().append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
+      .on("click", click)
+      .on("mouseover", hover)
+      .each(stash);
+
+  d3.selectAll(".second").on("change", function change() {
+    var value = this.value === "size"
+        ? function(d) { return d.size; }
+        : function() { return 1; };
+
+    path
+        .data(partition.value(value).nodes)
+      .transition()
+        .duration(1000)
+        .attrTween("d", arcTweenData);
+  });
+
+  function click(d) {
+    node = d;
+    path.transition()
+      .duration(1000)
+      .attrTween("d", arcTweenZoom(d));
+  }
+  function deshover(){
+  	$("#cwe").html("");
+      $("#high").html("");
+      $("#low").html("");
+      $("#cweAmount").html("");
+      $("#highAmount").html("");
+      $("#lowAmount").html("");
+  }
+
+  function hover(d) {
+    if(d.parent===undefined){
+      $("#cwe").html("");
+      $("#high").html("");
+      $("#low").html("");
+      $("#cweAmount").html("");
+      $("#highAmount").html("");
+      $("#lowAmount").html("");
+    }
+    else if(d.parent !== undefined && d.parent.parent === undefined){
+      $("#cwe").html(d.name);
+      $("#high").html("");
+      $("#low").html("");
+      $("#cweAmount").html(function(){return d.value+"/"+d.parent.value+" - "+((d.value*100)/d.parent.value).toString().substring(0,4)+"%"});
+      $("#highAmount").html("");
+      $("#lowAmount").html("");
+    }
+    else if(d.parent.parent !== undefined && d.parent.parent.parent === undefined){
+      $("#cwe").html(d.parent.name);
+      $("#high").html(d.name);
+      $("#low").html("");
+      $("#cweAmount").html(function(){return d.parent.value+"/"+d.parent.parent.value+" - "+((d.parent.value*100)/d.parent.parent.value).toString().substring(0,4)+"%"});
+      $("#highAmount").html(function(){return d.value+"/"+d.parent.parent.value+" - "+((d.value*100)/d.parent.parent.value).toString().substring(0,4)+"%"});
+      $("#lowAmount").html("");
+    }
+    else{
+      $("#cwe").html(d.parent.parent.name);
+      $("#high").html(d.parent.name);
+      $("#low").html(d.name);
+      $("#cweAmount").html(function(){return d.parent.parent.value+"/"+d.parent.parent.parent.value+" - "+((d.parent.parent.value*100)/d.parent.parent.parent.value).toString().substring(0,4)+"%"});
+      $("#highAmount").html(function(){return d.parent.value+"/"+d.parent.parent.parent.value+" - "+((d.parent.value*100)/d.parent.parent.parent.value).toString().substring(0,4)+"%"});
+      $("#lowAmount").html(function(){return d.value+"/"+d.parent.parent.parent.value+" - "+((d.value*100)/d.parent.parent.parent.value).toString().substring(0,4)+"%"});
+    }
+  }
+});
+
+d3.select(self.frameElement).style("height", height + "px");
+
+// Setup for switching data: stash the old values for transition.
+function stash(d) {
+  d.x0 = d.x;
+  d.dx0 = d.dx;
+}
+
+// When switching data: interpolate the arcs in data space.
+function arcTweenData(a, i) {
+  var oi = d3.interpolate({x: a.x0, dx: a.dx0}, a);
+  function tween(t) {
+    var b = oi(t);
+    a.x0 = b.x;
+    a.dx0 = b.dx;
+    return arc(b);
+  }
+  if (i == 0) {
+   // If we are on the first arc, adjust the x domain to match the root node
+   // at the current zoom level. (We only need to do this once.)
+    var xd = d3.interpolate(x.domain(), [node.x, node.x + node.dx]);
+    return function(t) {
+      x.domain(xd(t));
+      return tween(t);
+    };
+  } else {
+    return tween;
+  }
+}
+
+// When zooming: interpolate the scales.
+function arcTweenZoom(d) {
+  var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
+      yd = d3.interpolate(y.domain(), [d.y, 1]),
+      yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
+  return function(d, i) {
+    return i
+        ? function(t) { return arc(d); }
+        : function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
+  };
+}
